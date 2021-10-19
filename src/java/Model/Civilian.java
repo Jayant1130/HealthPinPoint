ca0fe2599bd4ba109383276c2d4ffdce58f2c915
Jayant1130 <jayantprajapat1130@gmail.com>
2021-10-19 16:48:26 +0530

    update folder structure according to MVC model

---
+++ src/java/Model/Civilian.java
@@ -0,0 +1,104 @@
+package Model;
+
+public class Civilian {
+        private int HealthID, DistrictID;
+        private String Name, MobileNo, Address, DOB, EmergencyNo, Photo, Email, Religion, Diet, MeritalStatus;   
+
+    public int getHealthID() {
+        return HealthID;
+    }
+
+    public void setHealthID(int HealthID) {
+        this.HealthID = HealthID;
+    }
+
+    public int getDistrictID() {
+        return DistrictID;
+    }
+
+    public void setDistrictID(int DistrictID) {
+        this.DistrictID = DistrictID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getMobileNo() {
+        return MobileNo;
+    }
+
+    public void setMobileNo(String MobileNo) {
+        this.MobileNo = MobileNo;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String Address) {
+        this.Address = Address;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public String getEmergencyNo() {
+        return EmergencyNo;
+    }
+
+    public void setEmergencyNo(String EmergencyNo) {
+        this.EmergencyNo = EmergencyNo;
+    }
+
+    public String getPhoto() {
+        return Photo;
+    }
+
+    public void setPhoto(String Photo) {
+        this.Photo = Photo;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getReligion() {
+        return Religion;
+    }
+
+    public void setReligion(String Religion) {
+        this.Religion = Religion;
+    }
+
+    public String getDiet() {
+        return Diet;
+    }
+
+    public void setDiet(String Diet) {
+        this.Diet = Diet;
+    }
+
+    public String getMeritalStatus() {
+        return MeritalStatus;
+    }
+
+    public void setMeritalStatus(String MeritalStatus) {
+        this.MeritalStatus = MeritalStatus;
+    }
+        
+        
+}
---
+++ src/java/Model/Login.java
@@ -0,0 +1,23 @@
+package Model;
+
+public class Login {
+    private int UserID;
+    private String Password;
+
+    public int getUserID() {
+        return UserID;
+    }
+
+    public void setUserID(int UserID) {
+        this.UserID = UserID;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+    
+}
